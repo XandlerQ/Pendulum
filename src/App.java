9ccfebc2c548bd6beaec107c078ef9ed16ae8d73
Alexander Brovko <brovkoav@student.bmstu.ru>
2023-09-02 12:48:28 +0300

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import processing.core.*;
+import controlP5.*;
+
+public class App extends PApplet{
+    public void settings() {
+
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
